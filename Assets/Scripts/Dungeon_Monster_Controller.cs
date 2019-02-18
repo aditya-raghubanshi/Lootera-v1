1d551608a35ae16aa61a69378fc2f4859f31836b
Aakash Dhongade <dhongadeaakash@gmail.com>
2019-02-18 04:09:31 -0800

    solved Ethan`s non-movement error and added dungeon monster again

---
+++ Assets/Scripts/Dungeon_Monster_Controller.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+public class Dungeon_Monster_Controller : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform goal;
+    CharacterController controller;
+    Animator anim;
+    float attackRadius;
+    float visibleRadius = 20;
+    public int enemyHealth = 10;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        attackRadius = agent.stoppingDistance;
+        print("Dungeon Monster in Start");
+       
+    }
+    public void Update()
+    {
+        Vector3 PlayerPosition = GameObject.Find("Player").transform.position;
+        float dist = Vector3.Distance(transform.position, PlayerPosition);
+        print("distance " + dist);
+        // When are we walking? - when we can see him and we are not attacking;
+        if(dist<visibleRadius)
+        {
+            print("Inside Visible Radius");
+            // when to stop walking when we are walking.
+            // if we are in attack radius 
+            if(dist<attackRadius)
+            {
+                print("Inside Attack");
+                Attack();
+            }
+            // if he is out of visible radius. 
+            else if(dist>visibleRadius)
+            {
+                Idle();
+            }
+            else
+            {
+                Walk(PlayerPosition);
+            }
+            
+        }
+        // When to become Idle - don`t really care which state we are in, if he is out of range just stop.
+
+        if(dist>visibleRadius)
+        {
+            Idle();
+        }
+
+        // When to Attack
+        if (dist<attackRadius)
+        {
+            
+            Attack();
+        }
+        // if my health is 0; then die
+      
+
+    }
+    void Walk(Vector3 PlayerPosition)
+    {
+
+        anim.SetBool("running", true);
+        anim.SetInteger("condition", 1);
+        anim.SetBool("attacking", false);
+        agent.SetDestination(PlayerPosition);
+        this.transform.LookAt(PlayerPosition);
+        controller.Move(agent.desiredVelocity * Time.deltaTime);
+    }
+    void Attack()
+    {
+        print("Inside attack function");
+        anim.SetBool("running", false);
+        anim.SetInteger("condition", 2);
+        anim.SetBool("attacking", true);
+        controller.Move(Vector3.zero);
+        agent.SetDestination(Vector3.zero);
+    }
+    void Idle()
+    {
+        anim.SetBool("running", false);
+        anim.SetInteger("condition", 0);
+        anim.SetBool("attacking", false);
+        controller.Move(Vector3.zero);
+        agent.SetDestination(Vector3.zero);
+    }
+ 
+    //}
+}
---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyManager : MonoBehaviour
+{
+
+    public GameObject [] enemies;                // The enemy prefab to be spawned.
+    public float spawnTime = 3f;            // How long between each spawn.
+    public float SpawnRadius = 10;
+
+    void Start()
+    {
+        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+        
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn()
+    {
+        // If the player has no health left...
+
+        // Find a random index between zero and one less than the number of spawn points.
+        int enemyIndex = Random.Range(0, enemies.Length);
+        Quaternion rot = GameObject.Find("Player").transform.rotation;
+        rot.y = System.Math.Abs(360 - rot.y);
+
+        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+        Instantiate(enemies[enemyIndex], RandomNavmeshLocation(SpawnRadius),rot );
+    }
+
+    public Vector3 RandomNavmeshLocation(float radius)
+    {
+        while (true)
+        {
+            Vector3 randomDirection = Random.insideUnitSphere * radius;
+            randomDirection += transform.position;
+            NavMeshHit hit;
+            Vector3 finalPosition = Vector3.zero;
+            if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1))
+            {
+                finalPosition = hit.position;
+                return finalPosition;
+            }
+        }
+
+        
+    }
+}
