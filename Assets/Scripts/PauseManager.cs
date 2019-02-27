5d59321aa527a53088e5f06beeb1d3c98904c476
sachinh2503 <36029196+sachinh2503@users.noreply.github.com>
2019-02-26 23:44:14 -0800

    Replaced master code with Pankti's zip file

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+    }
+
+    // Update is called once per frame
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+    }
+}
